409d473f54fcf20971406ecba11dc97babd66274
Spoozy <58841799+Spoozy@users.noreply.github.com>
2021-06-02 22:02:04 +0200

    changed the purpose of the bot to a lightweight moderation bot

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,24 @@
+const Discord = require('discord.js');
 module.exports = {
     name: 'help',
-    description: "this is a help command",
-    execute(message, args){
-        message.channel.send("Commands: Pog ping, Pog hello, Pog modapplication, Pog help (this command) and a secret command")
+    description: "embed!",
+    execute(message, args, Discord){
+        const newEmbed = new Discord.MessageEmbed()
+            .setColor('ff0000')
+            .setTitle('Help')
+            .setThumbnail('https://cdn.betterttv.net/emote/5e4e7a1f08b4447d56a92967/3x')
+            .setAuthor('Spoozy')
+            .setDescription('The commands assigned to this bot are:')
+            .addFields(
+                { name: 'Pog ban', value: 'Bans a member of your choice'},
+                { name: 'Pog kick', value: 'Kicks a member of your choice'},
+                { name: 'Pog ping', value: 'A command responding "Pong", used for testing purposes'},
+                { name: 'Pog mute', value: 'Mutes a member of your choice, add a number followed by "s, m, h, d" to time the mute'},
+                { name: 'Pog clear', value: 'Clears messages of a n argument'},
+            )
+            .setImage('https://cdn.betterttv.net/emote/5ff827395ef7d10c7912c106/3x')
+            .setTimestamp()
+            .setFooter('If you have any questions, please contact the mods in private messages');
+            message.channel.send(newEmbed);
     }
 }
